0103265fde3b226bf80781adf7ff07f36c9334e6
ryokosuge <ryo.kosuge@gmail.com>
2021-06-24 11:12:58 +0900

    chore:	update

--- AdMobApp/AppDelegate.swift
+++ AdMobApp/AppDelegate.swift
@@ -7,20 +7,40 @@
 //
 
 import UIKit
+import AppTrackingTransparency
+import AdSupport
 import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        GADMobileAds.sharedInstance().start { status in
-            print("==========")
-            status.adapterStatusesByClassName.forEach { (value) in
-                print("key:     \(value.key)")
-                print("status:  \(value.value)")
+        // Override point for customization after application launch
+
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { _ in
+                GADMobileAds.sharedInstance().start { status in
+                    print("==========")
+                    status.adapterStatusesByClassName.forEach { (value) in
+                        print("key:     \(value.key)")
+                        print("status:  \(value.value)")
+                    }
+                    print("==========")
+                }
+                let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+                print("idfa:    \(idfa)")
             }
-            print("==========")
+        } else {
+            GADMobileAds.sharedInstance().start { status in
+                print("==========")
+                status.adapterStatusesByClassName.forEach { (value) in
+                    print("key:     \(value.key)")
+                    print("status:  \(value.value)")
+                }
+                print("==========")
+            }
+            let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+            print("idfa:    \(idfa)")
         }
         return true
     }
